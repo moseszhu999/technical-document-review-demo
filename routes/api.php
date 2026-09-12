e68c6f5970f40437e2cd0ee4988c6347f57bbb81
moseszhu999 <138568031+moseszhu999@users.noreply.github.com>
2026-09-12 11:42:43 +0800

    補完 Knowledge Base と Rule Catalog の入力判断結果を公開

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\DemoChatController;
+use App\Http\Controllers\DemoKnowledgeController;
 use App\Http\Controllers\DemoReviewController;
+use App\Http\Controllers\DemoRuleCatalogController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/demo/review', DemoReviewController::class);
+Route::get('/demo/knowledge', DemoKnowledgeController::class);
+Route::get('/demo/rules', DemoRuleCatalogController::class);
+Route::post('/demo/chat', DemoChatController::class);
